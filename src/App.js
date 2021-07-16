bb5dc0e995d09af53612afd7d855d3e211d13b16
Prithiviraj Murugan (RBEI/ECD42-EC) <Murugan.Prithiviraj@in.bosch.com>
2021-07-16 16:07:20 +0530

    Final Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import './App.css'
+import Navbar from './Components/Navbar';
+import Home from './Components/pages/Home';
+import Products from './Components/pages/products';
+import Services from './Components/pages/Services';
+import SignUp from './Components/pages/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/products'  component={Products} />
+                    <Route path='/services' component={Services} />
+                    <Route path="/sign-up" component={SignUp} />
+                    
+                </Switch>
+            </Router>
+       </>
+    );
 }
 
 export default App;
